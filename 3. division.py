24772731df00a84e1677af954f58db13b6508b4a
tarun8295747673 <tarunkumar2322806@gmail.com>
2024-11-08 18:29:06 +0530

    Create 3. division.py

---
+++ 3. division.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num2 == 0:
+    print("Division by zero is not possible")
+else:
+    print("Division of", num1, "and", num2, "is", num1 / num2)
