fe9b8160489c9e0ddaea0703f5fe0976ac5420c6
tarun8295747673 <tarunkumar2322806@gmail.com>
2024-11-09 13:55:15 +0530

    Create 5. swap two no using third variable.py

---
+++ 5. swap two no using third variable.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Before swapping: a =", a, "and b =", b)
+
+temp = a
+a = b
+b = temp
+
+print("After swapping: a =", a, "and b =", b)
